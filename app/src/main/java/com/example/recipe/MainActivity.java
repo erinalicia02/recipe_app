e081b6541961f91df2a36260b7baf43639304749
Cfomodz <david.ashby.lds@gmail.com>
2020-01-27 20:50:03 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/recipe/MainActivity.java
+++ app/src/main/java/com/example/recipe/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //erin's 
+        //erin's //davids
     }
 }
