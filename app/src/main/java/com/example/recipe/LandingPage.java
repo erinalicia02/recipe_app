629762c2bc6cc6e60c87cf52d11350cb11514e04
Cfomodz <david.ashby.lds@gmail.com>
2020-03-07 19:22:56 -0700

    recipe class

---
+++ app/src/main/java/com/example/recipe/LandingPage.java
@@ -0,0 +1,9 @@
+package com.example.recipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+//public class LandingPage extends AppCompatActivity {
+//
+//}
