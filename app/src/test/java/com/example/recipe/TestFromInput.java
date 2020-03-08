4fe2405e5207bc7ad855cfdb9a122c39eba56fbe
Cfomodz <david.ashby.lds@gmail.com>
2020-03-07 18:52:19 -0700

    recipe class

---
+++ app/src/test/java/com/example/recipe/TestFromInput.java
@@ -0,0 +1,11 @@
+package com.example.recipe;
+
+import org.junit.Test;
+
+//public class TestFromInput {
+    //@Test
+    //Filler Data
+    //public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        //assertThat(EmailValidator.isValidEmail("name@email.com")).isTrue();
+   //}
+//}
